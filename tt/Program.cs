a131c7527af2866ffe28cc6ea05dbbb6814dab35
wellbye <7355233@qq.com>
2019-07-22 09:56:01 +0800

    fix

---
+++ tt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            return;
+            Console.WriteLine("Hello Again!");
+        }
+    }
+}
